32ea50c959b4b4516752273256109aa9fbf85862
Iselito22 <israelcolta@gmail.com>
2023-05-10 09:51:14 +0200

    He metido la carpeta Intranet dentro de la web

---
+++ CAFTÉ RETO/web/Intranet/Intranet_Comite/js/comite.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.video').forEach(vid => {
+    vid.onclick= () =>{
+        document.querySelector('.popup-video').style.display = 'block'
+        document.querySelector('.popup-video').src = vid.getAttribute('src')
+    }
+});
+
+document.querySelector('.popup-video span').onclick = () =>{
+    document.querySelector('.popup-video').style.display = 'none'
+}
