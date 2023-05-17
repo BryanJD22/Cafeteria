d205dfb4eb7f2f286a8be1de04a27d38e454e5ea
aalexcn <a26516@sanvalero.net>
2023-05-17 11:58:47 +0200

    mas

--- CAFTÉ RETO/web/js/comite.js
+++
@@ -1,7 +0,0 @@
-// Recuperar el nombre de usuario almacenado en sessionStorage
-var username = sessionStorage.getItem('username');
-
-// Mostrar el nombre de usuario en tu página web
-document.getElementById('usernameElement').textContent = username;
-
-
